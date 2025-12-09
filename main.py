af4797ee7c23fe588bf67a2e7a06f5cfb97b245c
Joshyers <joshuatimau@proton.me>
2025-12-09 08:59:23 +0100

    B: add circle shape and fps

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
     while True:
         log_state()
+        dt = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +18,8 @@ def main():
     
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60) # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("Screen width:", SCREEN_WIDTH)
