e9ec202317f93892f5e79b8f3593d646be9d2727
Joshyers <joshuatimau@proton.me>
2025-12-11 08:42:53 +0100

    F: added collision with asteroids

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import sys
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -30,6 +31,12 @@ def main():
                 return
         
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if player.collide_with(asteroid):
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
 
         screen.fill("black")
             
